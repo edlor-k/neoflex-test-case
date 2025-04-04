204155ae3b9e8d2fa88ca1318479eb986e52375a
edlor-k <korablev.vlm@gmail.com>
2025-04-04 14:18:05 +0300

    fix: correction for new logic

--- src/test/java/ru/korablev/spring/neoflextestcase/NeoflexTestCaseApplicationTests.java
+++ src/test/java/ru/korablev/spring/neoflextestcase/NeoflexTestCaseApplicationTests.java
@@ -15,14 +15,10 @@ import java.util.Objects;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class NeoflexTestCaseApplicationTests {
 
     private final TestRestTemplate restTemplate;
-
-    @Autowired
-    public NeoflexTestCaseApplicationTests(TestRestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
     private void assertVacationPay(double salary, int days, double expected) {
         ResponseEntity<VacationPayResponse> response =
@@ -35,17 +31,17 @@ class NeoflexTestCaseApplicationTests {
 
     @Test
     void testCalculateVacationPayWithoutStartDate() {
-        assertVacationPay(20600, 1, 1000.00);
+        assertVacationPay(29300, 1, 1000.00);
     }
 
     @Test
     void testCalculateVacationPayWithoutStartDateMultipleDays() {
-        assertVacationPay(20600, 5, 5000.00);
+        assertVacationPay(29300, 5, 5000.00);
     }
 
     @Test
     void testCalculateVacationPayWithStartDateFourteenDays() {
-        assertVacationPay(100000, 14, 67961.04);
+        assertVacationPay(100000, 14, 47781.44);
     }
 
     @Test
@@ -59,13 +55,13 @@ class NeoflexTestCaseApplicationTests {
                 restTemplate.getForEntity("/calculate?averageSalary={salary}&vacationDays={days}&startDate={date}",
                         VacationPayResponse.class, request.getAverageSalary(),
                         request.getVacationDays(), request.getStartDate());
-        assertThat(Objects.requireNonNull(response.getBody()).getTotalVacationPay()).isEqualTo(4854.36);
+        assertThat(Objects.requireNonNull(response.getBody()).getTotalVacationPay()).isEqualTo(3412.96);
     }
 
     @Test
     void testCalculateVacationPayWithStartDateMultipleDays() {
         VacationPayRequest request = new VacationPayRequest();
-        request.setAverageSalary(20600);
+        request.setAverageSalary(29300);
         request.setVacationDays(7);
         request.setStartDate(LocalDate.of(2025, 4, 25));
 
